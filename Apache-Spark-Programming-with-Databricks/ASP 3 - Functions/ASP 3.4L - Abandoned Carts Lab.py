7974ea3600e830440eae6dfd7ab7cbd0656292b5
atanu-c <atanu-c>
2022-08-09 12:07:44 +0000

    3.4L

--- Apache-Spark-Programming-with-Databricks/ASP 3 - Functions/ASP 3.4L - Abandoned Carts Lab.py
+++ Apache-Spark-Programming-with-Databricks/ASP 3 - Functions/ASP 3.4L - Abandoned Carts Lab.py
@@ -62,7 +62,7 @@ display(events_df)
 # TODO
 from pyspark.sql.functions import *
 
-converted_users_df = (sales_df.FILL_IN
+converted_users_df = (sales_df.select("email").drop_duplicates(["email"]).withColumn("converted", lit(True))
                      )
 display(converted_users_df)
 
@@ -97,8 +97,10 @@ print("All test pass")
 
 # COMMAND ----------
 
-# TODO
-conversions_df = (users_df.FILL_IN
+conversions_df = (users_df
+                  .join(converted_users_df, "email", "outer")
+                  .filter(col("email").isNotNull())
+                  .na.fill(value=False,subset=["converted"])
                  )
 display(conversions_df)
 
@@ -138,7 +140,8 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-carts_df = (events_df.FILL_IN
+carts_df = (events_df.withColumn("items", explode("items"))
+            .groupBy("user_id").agg(collect_set("items.item_id").alias("cart"))
 )
 display(carts_df)
 
@@ -171,8 +174,7 @@ print("All test pass")
 
 # COMMAND ----------
 
-# TODO
-email_carts_df = conversions_df.FILL_IN
+email_carts_df = conversions_df.join(carts_df, "user_id", "left")
 display(email_carts_df)
 
 # COMMAND ----------
@@ -208,7 +210,8 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-abandoned_carts_df = (email_carts_df.FILL_IN
+abandoned_carts_df = (email_carts_df.filter(col("converted")==False)
+                      .filter(col("cart").isNotNull())
 )
 display(abandoned_carts_df)
 
@@ -238,7 +241,7 @@ print("All test pass")
 # COMMAND ----------
 
 # TODO
-abandoned_items_df = (abandoned_carts_df.FILL_IN
+abandoned_items_df = (abandoned_carts_df.withColumn("items", explode("cart")).select("items").groupBy("items").count()
                      )
 display(abandoned_items_df)
 
